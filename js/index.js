51e401e6e36e84f1528fe3372643b69171b81179
eugeisworking <91647157+eugeisworking@users.noreply.github.com>
2023-05-28 19:38:58 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,52 @@
+// Esta linea garantiza que todo lo que se ejecute dentro de ready
+// se ejecute solo cuando el html esté 100% cargado
+$(document).ready(() => {
+  console.log("JQuery Cargado");
+
+  // Cuando hacemos click en "suscribirme" de los form abreviados, abrimos el popup del form completo
+  $(".miniform-submit").click(() => {
+    abrirPopupForm();
+    trasladarMailDeMiniformAFormCompleto();
+  });
+
+  // Cuando hacemos click en la cruz de los popup, los cerramos
+  $(".cerrar").click(() => {
+    cerrarPopup();
+  });
+
+  // Cuando enviamos el formulario completo, mostramos el feedback
+  $("#formulario-principal").on("submit", (e) => {
+    e.preventDefault(); // Esta linea es importante para evitar que la página se recargue al enviar le formulario
+    abrirFeedback();
+    trasladarDatosDelFormCompletoAFeedback();
+  });
+});
+
+function abrirPopupForm() {
+
+}
+
+function cerrarPopup() {
+
+}
+
+function abrirFeedback() {
+  $(".popup-contenedor.formulario").addClass("oculto");
+  $(".popup-contenedor.feedback").removeClass("oculto");
+
+}
+
+function trasladarDatosDelFormCompletoAFeedback() {
+  const inputNombre = $("#nombre");
+  const inputEmail = $("#email");
+
+  const nombre = inputNombre.val();
+  const email = inputEmail.val();
+
+  const spanNombre = $("#dato-nombre");
+  const spanEmail = $("#dato-email");
+
+  spanNombre.html(nombre + " " + apellido);
+  spanEmail.html(email);
+}
+
